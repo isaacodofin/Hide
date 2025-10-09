52c303fe18db4cc38f2fd7174b821fbb1e0c51e2
isaacodofin <eminentboy12345@gmail.com>
2025-10-10 00:13:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -95,14 +95,14 @@ const contextSenderIsSudo = tempContext.senderIsSudo;
         message.message?.videoMessage?.caption?.trim() ||  
         '';  
        // === LOG ALL MESSAGES ===
-console.log(rainbow(`
+console.log(rainbow(`n/
 ‎┏‎━━━━━━━━[GIFT-MD]‎━━━━━━━━┓
 ┃ 📩 New Message
 ┃ 📍 Chat: ${isGroup ? "𝗚𝗿𝗼𝘂𝗽" : isChannel ? "𝗖𝗵𝗮𝗻𝗻𝗲𝗹" : "𝗣𝗿𝗶𝘃𝗮𝘁𝗲"}
 ┃ 🆔 Chatid: ${chatId}
 ┃ 👤 Sender: ${senderId}
 ┃ 💌 Text: ${rawText || "[𝗠𝗲𝗱𝗶𝗮/𝗦𝘁𝗶𝗰𝗸𝗲𝗿/𝗢𝘁𝗵𝗲𝗿]"}
-┗‎━━━━━━━━━‎━━━━━━━━━‎━━━━━━┛`));
+┗‎━━━━━━━━━‎━━━━━━━━━‎━━━━━━┛n/n/`));
     // Only log command usage  
     if (userMessage.startsWith(currentPrefix)) {  
         // ✅ FIXED: Reduced auto-reactions to avoid rate limits  
