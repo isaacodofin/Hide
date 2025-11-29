ad6625004f9e2477117ba84c6d98964625c1ee76
isaacodofin <eminentboy12345@gmail.com>
2025-11-29 04:41:50 +0100

    Create Quote.js

---
+++ Quote.js
@@ -0,0 +1,31 @@
+//=======°°°°°°°°°°′°°°°=========//
+
+
+/**1*/ let menu = {
+  key: {
+    remoteJid: '0@s.whatsapp.net',
+    fromMe: false,
+    participant: '0@s.whatsapp.net'
+  },
+  message: {
+    extendedTextMessage: {
+      text: "🇳🇬:𝗚𝗜𝗙𝗧_𝗠𝗗_.𝗠𝗘𝗡𝗨"
+    }}};
+
+global.m = menu;
+
+/**2*/ let alive = {
+  key: {
+    remoteJid: '0@s.whatsapp.net',
+    fromMe: false,
+    participant: '0@s.whatsapp.net'
+  },
+  message: {
+    extendedTextMessage: {
+      text: "🇳🇬:𝗚𝗜𝗙𝗧_𝗠𝗗_.𝗠𝗘𝗡𝗨"
+    }
+  }
+};
+
+global.A = alive;
+
