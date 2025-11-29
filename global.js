595d354019ca57cf57c95cae54467094f1a0da58
isaacodofin <eminentboy12345@gmail.com>
2025-11-29 22:56:51 +0100

    Create global.js

---
+++ global.js
@@ -0,0 +1,40 @@
+//=======°°°°°°°°°°′°°°°=========//
+
+
+/**1*/ global.M= {
+  key: {
+    remoteJid: '0@s.whatsapp.net',
+    fromMe: false,
+    participant: '0@s.whatsapp.net'
+  },
+  message: {
+    extendedTextMessage: {
+      text: `🇳🇬:𝗚𝗜𝗙𝗧_𝗠𝗗:🇳🇬\n.𝗠𝗘𝗡𝗨`
+    }}};
+
+/**2*/ global.A= {
+  key: {
+    remoteJid: '0@s.whatsapp.net',
+    fromMe: false,
+    participant: '0@s.whatsapp.net'
+  },
+  message: {
+    extendedTextMessage: {
+      text: "🇳🇬:𝗚𝗜𝗙𝗧_𝗠𝗗_.𝗠𝗘𝗡𝗨"
+    }
+  }
+};
+
+
+global. h= {
+  key: {
+    remoteJid: '0@s.whatsapp.net',
+    fromMe: false,
+    participant: '0@s.whatsapp.net'
+  },
+  message: {
+    extendedTextMessage: {
+      text: "🇳🇬:𝗚𝗜𝗙𝗧_𝗠𝗗_🇳🇬"
+    }
+  }
+};
